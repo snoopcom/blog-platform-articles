1b0c89539e64c290a7e5f1543ecf64b1470e6597
Alex <snoopcom@bk.ru>
2020-08-17 23:15:28 +0300

    final

--- src/components/header/style.js
+++ src/components/header/style.js
@@ -10,12 +10,17 @@ export const List = styled.ul`
   display: flex;
   justify-content: space-between;
   margin-left: auto;
+  padding-right: 30px;
+  padding-top: 20px;
   list-style: none;
   border: 1px red solid;
-  width: 200px;
-  height: 70px;
+  width: 450px;
+  height: 80px;
   text-align: ;
 `;
 export const Item = styled.li`
   border: 1px red solid;
+  margin-right: 30px;
+  width: 200px;
+  height: 40px;
 `;
