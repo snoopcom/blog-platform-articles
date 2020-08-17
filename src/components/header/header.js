579232fbf372b909620430179bb7b2c1c02f1d3a
Alex <snoopcom@list.ru>
2020-08-17 17:32:02 +0400

    test

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,18 +1,38 @@
 import React from 'react';
-import { Button } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { Wrapper, Item, List } from './style';
+import { isInactive } from '../../store/actions';
 
-const Header = () => (
-  <Wrapper>
-    <List>
-      <Item>
-        <Button>SignUp</Button>
-      </Item>
-      <Item>
-        <Button>Login</Button>
-      </Item>
-    </List>
-  </Wrapper>
-);
+const Header = () => {
+  const buttonReducer = useSelector((state) => state.buttonReducer);
+  const dispatch = useDispatch();
+
+  const handleSignUp = () => {
+    dispatch(isInactive);
+    console.log();
+  };
+  const handleLogin = () => {
+    dispatch(isInactive);
+    console.log();
+  };
+
+  return (
+    <Wrapper>
+      <List disabled={buttonReducer}>
+        <Item>
+          <Link to="/signUp" onClick={handleSignUp}>
+            SignUp
+          </Link>
+        </Item>
+        <Item>
+          <Link to="/login" onClick={handleLogin}>
+            Login
+          </Link>
+        </Item>
+      </List>
+    </Wrapper>
+  );
+};
 
 export default Header;
